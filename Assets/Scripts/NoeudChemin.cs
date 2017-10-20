5c3982fd12dc0d63168499eb7063bd92ffc792dd
Argann Bonneau <argann.b@hotmail.fr>
2017-10-20 23:16:08 +0200

    Création DeplacementEmploye & NoeudChemin

---
+++ Assets/Scripts/NoeudChemin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoeudChemin : MonoBehaviour {
+
+    [SerializeField]
+    private List<NoeudChemin> voisins;
+
+    public List<NoeudChemin> Voisins {
+        get { return voisins; }
+        set { voisins = value; }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
