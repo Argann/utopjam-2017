fa156d1c5d513fc368bd4da1ed524b5b6bd30280
Argann Bonneau <argann.b@hotmail.fr>
2017-10-20 21:35:39 +0200

    Création du script de déplacement générique
    Ce script permet de déplacer via le code un objet ayant un rigidbody2d.
    Suppression du fichier `empty` du dossier Scripts

---
+++ Assets/Scripts/Deplacement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Deplacement : MonoBehaviour {
+
+    /// <summary>
+    /// Rigidbody du joueur.
+    /// </summary>
+    private Rigidbody2D rb;
+
+    /// <summary>
+    /// Vitesse de déplacement de l'entité.
+    /// </summary>
+    [SerializeField]
+    [Range(0,50f)]
+    private float speed;
+
+    public float Speed {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+    // Appelé lors de l'apparition de l'objet attaché.
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Permet de déplacer l'objet dans une direction précise.
+    /// Elle doit être appelée à chaque boucle d'affichage.
+    /// </summary>
+    /// <param name="direction">
+    /// Vector2 représentant la direction vers laquelle l'objet doit se déplacer
+    /// </param>
+    public void Move(Vector2 direction) {
+        rb.velocity = direction * speed;
+    }
+
+
+
+}
