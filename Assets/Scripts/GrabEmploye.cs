e1e48f14390797c532089db49347506605891fa0
Argann Bonneau <argann.b@hotmail.fr>
2017-10-21 11:56:11 +0200

    On peut prendre un employé, pour le ramener sur son poste

---
+++ Assets/Scripts/GrabEmploye.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class GrabEmploye : MonoBehaviour {
+
+    private bool tientEmploye;
+
+    void Start() {
+        tientEmploye = false;
+    }
+
+	void OnTriggerStay2D(Collider2D coll) {
+        if (coll.CompareTag("Employe") && Input.GetAxisRaw("Jump") > 0 && !tientEmploye) {
+            PrendEmploye();
+        }
+
+        if (coll.CompareTag("Poste") && Input.GetAxisRaw("Jump") > 0 && tientEmploye) {
+            PoseEmploye();
+        }
+    }
+
+    public void PrendEmploye() {
+        tientEmploye = true;
+    }
+
+    public void PoseEmploye() {
+        tientEmploye = false;
+    }
+}
