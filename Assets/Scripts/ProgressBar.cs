7187329a45edda88458e81808f986408837eacf3
pierrebigaud <pierrebigaud44@orange.fr>
2017-10-21 21:37:27 +0200

    maintenant le score et la progressbar mene a la fin pierre

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ProgressBar : MonoBehaviour {
-	// Speed by employee, of the evolution of the game.
+    // Speed by employee, of the evolution of the game.
+    public Ptimer Temps;
 	public float vitesse;
 
 	// Set to 0, depends on how many employees are actually working.
@@ -27,10 +29,15 @@ public class ProgressBar : MonoBehaviour {
 		if ( slider.value >= MAX ) {
 			vitesse = -vitesse;
 			ProgressBar.IncrementEmployees();
-			// MOCK of end condition
-			Debug.Log("YOU WIN !!");
-		}
+            // MOCK of end condition
+            bonnesfins();
+        }
 	}
+
+    public float GetSliderValue()
+    {
+        return slider.value;
+    }
 
 	static void IncrementEmployees() {
 		nbEmployees++;
@@ -39,4 +46,14 @@ public class ProgressBar : MonoBehaviour {
 	static void DecrementEmployees() {
 		nbEmployees--;
 	}
+
+    void bonnesfins()
+    {
+        PlayerPrefs.SetInt("stade", 3);
+        PlayerPrefs.SetInt("heure", Temps.GetHeuresPasses());
+        PlayerPrefs.SetInt("minutes", Temps.GetMinutesPasses());
+        PlayerPrefs.SetInt("fin", (true ? 1 : 0));
+
+        SceneManager.LoadScene("GameOver");
+    }
 }
--- Assets/Scripts/Ptimer.cs
+++ Assets/Scripts/Ptimer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Ptimer : MonoBehaviour {
 
@@ -10,39 +11,65 @@ public class Ptimer : MonoBehaviour {
     [SerializeField]
     private float vitesseJeu;
 
+    private int minutes;
+
     private float timeStart;
 
     private float hourStart;
     private int hour;
-    private string[] jours= new string[]
+    private string[] jours = new string[]
     {
         "vendredi",
         "samedi",
         "dimanche"
     };
     private int jourStart;
+    public ProgressBar barreprogression;
 
-	// Use this for initialization
-	void Awake () {
+    private int jourpasses;
+
+    // Use this for initialization
+    void Awake() {
         timeStart = Time.time;
         hourStart = 18;
         jourStart = 0;
         timer = GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        jourpasses = 0;
+    }
+
+    public int GetHeuresPasses() 
+    {
+        if (jourpasses == 0)
+            return hour-18;
+        else if (jourpasses == 1)
+        {
+            return hour+6;
+        }
+        else
+        {
+            return hour + 30;
+        }
+    }
+
+    public int GetMinutesPasses()
+    {
+        return minutes;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (jourStart == 2 && hour >= 18)
         {
-            timer.text = "Game Over";
+            mauvaisesfins();
+            // timeStart = Time.time;
         }
         else
         {
             float t = (Time.time - timeStart) * vitesseJeu;
 
-            int minutes = (int)(t % 60);
+            minutes = (int)(t % 60);
 
-             hour = ((int)(hourStart + (t / 60)));
+            hour = ((int)(hourStart + (t / 60)));
 
             if (hour >= 24)
             {
@@ -50,6 +77,7 @@ public class Ptimer : MonoBehaviour {
                 hourStart = 0;
                 hour = 0;
                 jourStart++;
+                jourpasses++;
             }
 
             string jour = jours[jourStart];
@@ -57,4 +85,32 @@ public class Ptimer : MonoBehaviour {
             timer.text = jour + " " + hour + " " + minutes;
         }
 	}
+
+    void mauvaisesfins()
+    {
+        int stade;
+        float pourcentageprogression = barreprogression.GetSliderValue();
+        if (pourcentageprogression>=.75f)
+        {
+            stade = 3;
+        }
+        else if(pourcentageprogression >= .5f)
+        {
+            stade = 2;
+        }
+        else if (pourcentageprogression >= .25f)
+        {
+            stade = 1;
+        }
+        else
+        {
+            stade = 0;
+        }
+        PlayerPrefs.SetInt("stade", stade);
+        PlayerPrefs.SetInt("heure", 0);
+        PlayerPrefs.SetInt("minutes", 0);
+        PlayerPrefs.SetInt("fin", (false ? 1 : 0));
+
+        SceneManager.LoadScene("GameOver");
+    }
 }
