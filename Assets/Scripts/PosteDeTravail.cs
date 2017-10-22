2f0d4c1a8697c37255a62280f60ff5483f728d09
Argann Bonneau <argann.b@hotmail.fr>
2017-10-22 12:09:59 +0200

    Bugfix plusieurs employés sur un poste

---
+++ Assets/Scripts/PosteDeTravail.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosteDeTravail : MonoBehaviour {
+
+    [SerializeField]
+    private bool contientEmploye;
+
+    public bool ContientEmploye {
+        get { return contientEmploye; }
+        set { contientEmploye = value; }
+    }
+
+}
