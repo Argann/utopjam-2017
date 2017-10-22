8c72ce79e2f8934ca6abaf7aad70c072e5f8db29
Argann Bonneau <argann.b@hotmail.fr>
2017-10-22 09:36:09 +0200

    Ajout mouvement caméra via souris
    Et correction bug inputs.

--- Assets/Scripts/CameraPersonnage.cs
+++ Assets/Scripts/CameraPersonnage.cs
@@ -6,9 +6,17 @@ public class CameraPersonnage : MonoBehaviour {
 
     [SerializeField]
     private GameObject joueur;
+
+    [SerializeField]
+    private float maxCameraDistance;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(joueur.transform.position.x, joueur.transform.position.y, transform.position.z);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 distanceMousePlayer = (mousePos - joueur.transform.position).normalized;
+
+        transform.position = new Vector3(joueur.transform.position.x + distanceMousePlayer.x, joueur.transform.position.y + distanceMousePlayer.y, transform.position.z);
+
 	}
 }
